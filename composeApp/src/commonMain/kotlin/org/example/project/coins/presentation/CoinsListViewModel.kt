6ca6f9a671a4e06274a4e6314ccc6a64a32e7961
UĞUR YAŞAR KAZDAL <uykazdal@live.com>
2025-06-27 15:28:36 +0300

    coin history
    coin history chart completed

--- composeApp/src/commonMain/kotlin/org/example/project/coins/presentation/CoinsListViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/coins/presentation/CoinsListViewModel.kt
@@ -7,20 +7,23 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.example.project.coins.domain.GetCoinPriceHistoryUseCase
 import org.example.project.coins.domain.GetCoinsListUseCase
 import org.example.project.core.domain.Result
 import org.example.project.core.util.formatFiat
 import org.example.project.core.util.formatPercentage
 import org.example.project.core.util.toUiText
 
-class CoinsListViewModel(private val getCoinsListUseCase: GetCoinsListUseCase) : ViewModel() {
+class CoinsListViewModel(
+    private val getCoinsListUseCase: GetCoinsListUseCase,
+    private val getCoinPriceHistoryUseCase: GetCoinPriceHistoryUseCase
+) : ViewModel() {
     private val _state = MutableStateFlow(CoinState())
     val state = _state.onStart {
         getAllCoins()
     }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000),
-        _state.value
+        viewModelScope, SharingStarted.WhileSubscribed(5000), _state.value
     )
 
     private suspend fun getAllCoins() {
@@ -45,12 +48,52 @@ class CoinsListViewModel(private val getCoinsListUseCase: GetCoinsListUseCase) :
             is Result.Error -> {
                 _state.update {
                     it.copy(
-                        coins = emptyList(),
-                        error = coinsResponse.error.toUiText()
+                        coins = emptyList(), error = coinsResponse.error.toUiText()
                     )
                 }
             }
 
+        }
+    }
+
+    fun onCoinLongPressed(coinId: String) {
+        _state.update {
+            it.copy(
+                chartState = UiChartState(sparkLine = emptyList(), isLoading = true)
+            )
+        }
+
+        viewModelScope.launch {
+            when (val priceHistoryResponse = getCoinPriceHistoryUseCase.execute(coinId)) {
+                is Result.Success -> {
+                    _state.update { currentState ->
+                        currentState.copy(
+                            chartState = UiChartState(
+                                sparkLine = priceHistoryResponse.data?.sortedBy { it?.timestamp }
+                                    ?.map { it?.price ?: 0.0 } ?: emptyList(),
+                                isLoading = false,
+                                coinName = _state.value.coins.find { it.id == coinId }?.name.orEmpty()
+                            )
+                        )
+                    }
+                }
+
+                is Result.Error -> {
+                    _state.update {
+                        it.copy(
+                            chartState = UiChartState(
+                                sparkLine = emptyList(), isLoading = false
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun onDismissChart() {
+        _state.update {
+            it.copy(chartState = null)
         }
     }
 }
