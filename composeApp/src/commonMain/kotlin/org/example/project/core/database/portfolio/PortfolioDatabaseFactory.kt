fdbf4011eab0e848a6d068d7332d6e2c8812192e
uguryasarkazdal <uykazdal@live.com>
2025-06-28 14:03:45 +0300

    portfolio database created

---
+++ composeApp/src/commonMain/kotlin/org/example/project/core/database/portfolio/PortfolioDatabaseFactory.kt
@@ -0,0 +1,22 @@
+package org.example.project.core.database.portfolio
+
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object PortfolioDatabaseCreator : RoomDatabaseConstructor<PortfolioDatabase>
+
+
+fun getPortfolioDatabase(
+    builder: RoomDatabase.Builder<PortfolioDatabase>
+): PortfolioDatabase {
+    return builder
+        //.addMigrations()
+        //.fallbackToDestructiveMigrationOnDowngrade()
+        .setDriver(BundledSQLiteDriver())
+        .setQueryCoroutineContext(Dispatchers.IO)
+        .build()
+}
