fb8e795769c5111d3014f58890d39485f6336836
UĞUR YAŞAR KAZDAL <uykazdal@live.com>
2025-07-01 20:20:25 +0300

    sellcoinusecase added

--- composeApp/src/commonMain/kotlin/org/example/project/di/Module.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/di/Module.kt
@@ -15,6 +15,7 @@ import org.example.project.portfolio.data.PortfolioRepositoryImpl
 import org.example.project.portfolio.domain.PortfolioRepository
 import org.example.project.portfolio.presentation.PortfolioViewModel
 import org.example.project.trade.domain.BuyCoinUseCase
+import org.example.project.trade.domain.SellCoinUseCase
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -54,5 +55,6 @@ val sharedModule = module {
 
     //trade
     singleOf(::BuyCoinUseCase)
+    singleOf(::SellCoinUseCase)
 }
 
